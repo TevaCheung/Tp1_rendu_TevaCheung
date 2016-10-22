12c68b28e34fffb38d40166aa939aed37322d7b4
Teva <t.cheung@ludus-academie.com>
2016-10-22 18:36:09 +0200

    rendu 2 tp 1

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*MES 2 ERREURS PAR RAPPORT A MON PREMIER ENVOI :
+    -avoir ecrit = au lieu de == pour le test de comparaison logique
+    -ne pas avoir pris en compte la LIGNE EN COURS
+*/
 
 int main()
 {
@@ -13,25 +17,28 @@ int main()
     printf("Vous allez afficher un carre avec des cotes faits d etoiles et une aire de plus\n");
 
     printf("Entrez une longueur : ");
-    scanf("%d",&longueur);
-
-    printf("\nEntrez une largeur : ");
-    scanf("%d",&largeur);
-
+    scanf("%i",&longueur);
     fflush(stdin);
 
-    for(j=0;j<largeur;j++){
-        for(i=0;i<longueur;i++){
-            if(i=0){
-                ligne[i]='*';
-            }else{
-                ligne[i]='+';
-            }
-        }
+    printf("\nEntrez une largeur : ");
+    scanf("%i",&largeur);
+    fflush(stdin);
 
-        for (i=0;i<longueur;i++){
+    for(j=0;j<largeur;j++){ //LIGNE PAR LIGNE ...
+        for(i=0;i<longueur;i++){
+            if ((j==0) || (j==largeur-1)) { // S'IL S AGIT DE LA PREMIERE OU DERNIERE LIGNE, IL N Y AURA QUE DES ETOILES
+                ligne[i]='*';
+            }else{ //S IL S AGIT D UNE AUTRE LIGNE
+                if ((i==0) || (i==longueur-1)){ //S IL S AGIT DE LA PREMIERE OU DERNIERE COLONNE
+                    ligne[i]='*';
+                }else{                      //SI C EST UNE AUTRE COLONNE
+                    ligne[i]='+';
+                }
+            }
+
             printf("%c",ligne[i]);
         }
+
         printf("\n");
     }
 
